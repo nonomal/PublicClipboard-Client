deebea4768a0a86a3f6a83a26a5ae50c35711663
sxz799 <sxz15371@163.com>
2022-09-15 20:47:50 +0800

    1

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	lastContent, _ := clipboard.ReadAll()
-	if viper.GetString("sleepTime") == "1" {
+	if viper.GetString("initMode") == "1" {
 		clipboard.WriteAll(util.GetContent())
 	} else {
 		util.UpdContent(lastContent)
@@ -21,7 +21,6 @@ func main() {
 	fmt.Println("程序运行中...")
 	for {
 		time.Sleep(time.Millisecond * time.Duration(sleepTime))
-		log.Println("1111")
 		local, _ := clipboard.ReadAll()
 		remote := util.GetContent()
 		if local != "" && remote != "" && local != remote {
